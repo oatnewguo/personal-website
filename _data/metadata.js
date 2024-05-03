3fd353e99b4c155402fcec48e18a9a7f385ea377
Wentao Guo <wguo5@umd.edu>
2024-05-02 21:37:11 -0400

    Minor updates to bio

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "Wentao Guo",
 	url: "https://wentaoguo.com",
 	language: "en",
-	description: "Wentao Guo is a PhD student in Computer Science at the University of Maryland (UMD). He researches human-centered security and privacy and is interested in professionals who play a role in informing or protecting others. This website documents Wentao's research and other projects.",
+	description: "Wentao Guo is a PhD student in Computer Science at the University of Maryland (UMD). He researches human-centered security and privacy and is interested unlocking the power of professionals to protect users' security and privacy. This website documents Wentao's research and other projects.",
 	author: {
 		name: "Wentao Guo",
 		email: "wguo5@umd.edu",
