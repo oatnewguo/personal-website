89b744b539758dbb94dcdfd66e7fb52f6980ad4b
Wentao Guo <oatnewguo@gmail.com>
2026-08-03 16:19:13 -0400

    minor updates

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ export default {
 	title: "Wentao Guo",
 	url: "https://wentaoguo.com",
 	language: "en",
-	description: "Wentao Guo is a PhD student in Computer Science at the University of Maryland (UMD). He researches human-centered security and privacy and is interested unlocking the power of professionals to protect users' security and privacy. This website documents Wentao's research and other projects.",
+	description: "Wentao Guo is a postdoc at Tufts University, soon to be a professor in the School of Computing at the University of Georgia. He researches real-world challenges in computer security and privacy from a human-centered perspective, with a particular interest in practitioners who have disproportionate impacts on the security and privacy of ordinary users. This website documents Wentao's research and other projects.",
 	author: {
 		name: "Wentao Guo",
 		email: "wguo5@umd.edu",
